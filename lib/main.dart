c5e5a9d9ba189a6d3cba77fd336fc5f66cd9831d
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-02 12:15:32 +0200

    Refactor main.dart to use BlocBuilder for language state management

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_localization/bloc_observer.dart';
 import 'package:flutter_localization/generated/l10n.dart';
 import 'package:flutter_localization/home_screen.dart';
 import 'package:flutter_localization/logic/change_language_cubit.dart';
+import 'package:flutter_localization/logic/change_language_state.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'di/dependency_injection.dart';
@@ -20,24 +21,28 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      locale: Locale(
-        getIt<ChangeLanguageCubit>().currentLanguage,
-      ),
-      localizationsDelegates: [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: S.delegate.supportedLocales,
-      title: 'Flutter Localization',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    final changeLanguageCubit = getIt<ChangeLanguageCubit>();
+    return BlocBuilder<ChangeLanguageCubit, ChangeLanguageState>(
+      bloc: changeLanguageCubit,
+      builder: (context, state) {
+        return MaterialApp(
+          locale: Locale(changeLanguageCubit.currentLanguage),
+          localizationsDelegates: [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: S.delegate.supportedLocales,
+          title: 'Flutter Localization',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+            useMaterial3: true,
+          ),
+          debugShowCheckedModeBanner: false,
+          home: HomeScreen(),
+        );
+      },
     );
   }
 }
